e88142b90f026e6b59555c0b420e8d8503010c3b
James Shapiro <jamesshapiro@gmail.com>
2017-04-14 12:35:19 -0700

    add bipartite testing

--- lab5/graph.py
+++ lab5/graph.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3 -tt
 
 class Vertex:
-    def __init__(self, name=""):
+    def __init__(self, name="",color="gray"):
         self.name = name
         self.edges = set()
+        self.color = color
     def __str__(self):
-        return "{} {}".format(self.name, str(self.edges))
+        return "[name={}, edges={}, color={}]".format(self.name, str(self.edges), self.color)
     def __repr__(self):
         return str(self)
 
@@ -16,7 +17,7 @@ class Edge:
         self.cost = cost
         self.visited = visited
     def __str__(self):
-        return "{} {} {} {}".format(self.start.name, self.end.name,
+        return "[({}->{}) cost={} v={}]".format(self.start.name, self.end.name,
                                     self.cost, self.visited)
     def __repr__(self):
         return str(self)
@@ -68,6 +69,8 @@ class SimpleGraph:
     def clear_all(self):
         for edge in self.edges:
             edge.visited = False
+        for vert in self.verts:
+            vert.color = "gray"
     def is_reachable(self, v1, v2):
         edges_left = [edge for edge in v1.edges if edge.start == v1]
         while len(edges_left) > 0:
@@ -84,15 +87,42 @@ class SimpleGraph:
                                    if edge.start == nextVisited and not edge.visited])
         self.clear_all()
         return False
+    def is_bipartite(self):
+        verts = set(self.verts)
+        while len(verts) > 0:
+            stack = list()
+            v = verts.pop()
+            
+            stack.append(("black", v))
+            while len(stack) > 0:
+                par_color, v = stack.pop()
+                if v in verts:
+                    verts.remove(v)
+                if v.color == par_color:
+                    self.clear_all()
+                    return False
+                if v.color == "gray" and par_color == "black":
+                    v.color = "red"
+                elif v.color == "gray" and par_color == "red":
+                    v.color = "black"
+                neighbors = [e.start if e.start != v else e.end for e in v.edges]
+                for neighbor in neighbors:
+                    if neighbor.color == v.color:
+                        self.clear_all()
+                        return False
+                    if neighbor.color == 'gray':
+                        stack.append((v.color, neighbor))
+        self.clear_all()
+        return True
 
-""""""
+"""
 from graph import SimpleGraph
 from graph import Vertex
 from graph import Edge
+"""
 
 g = SimpleGraph()
-g.is_empty() == True
-result = g.is_empty()
+assert g.is_empty()
 
 a = Vertex('a')
 b = Vertex('b')
@@ -102,63 +132,89 @@ e = Vertex('e')
 f = Vertex('f')
 
 g.add_vertex(a)
-g.is_empty() == False
-result = g.is_empty()
+assert not g.is_empty()
 
 g.add_vertex(b)
 g.add_vertex(c)
 g.add_vertex(d)
 g.add_vertex(e)
 
-g.contains_vertex(a) == True
-result = g.contains_vertex(a)
-g.contains_vertex(f) == False
-result = g.contains_vertex(f)
+assert g.contains_vertex(a)
+assert not g.contains_vertex(f)
 
 g.add_edge(a, b)
 g.add_edge(a, e)
 g.remove_edge(a, e)
-g.size() == (5, 1)
-result = g.size()
+assert g.size() == (5, 1)
 
 g.add_edge(a, b)
-g.size() == (5, 1)
-result = g.size()
+assert g.size() == (5, 1)
 
 g.add_edge(b, c)
 g.add_edge(c, b)
 g.add_edge(c, d)
 
 b_neighbors = g.get_neighbors(b)
-len(b_neighbors) == 2
-result = len(b_neighbors)
-a in b_neighbors
-result = (a in b_neighbors)
-c in b_neighbors
-result = (c in b_neighbors)
+assert len(b_neighbors) == 2
+assert a in b_neighbors
+assert c in b_neighbors
 
-g.is_neighbor(a, b) == True
-result = g.is_neighbor(a, b)
-g.is_neighbor(a, d) == False
-result = g.is_neighbor(a, d)
-all([not g.is_neighbor(e, v) for v in g.verts]) == True
-result = all([not g.is_neighbor(e, v) for v in g.verts])
+assert g.is_neighbor(a, b)
+assert not g.is_neighbor(a, d)
+assert all([not g.is_neighbor(e, v) for v in g.verts])
 
-g.is_reachable(a, d) == True
-result = g.is_reachable(a, d)
-g.is_reachable(a, d) == True
-result = g.is_reachable(a, d)
+assert g.is_reachable(a, d)
+assert g.is_reachable(a, d)
 
-g.is_reachable(d, a) == False
-result = g.is_reachable(d, a)
-
-g.is_reachable(a, e) == False
-result = g.is_reachable(a, e)
+assert not g.is_reachable(d, a)
+assert not g.is_reachable(a, e)
 
 g.remove_vertex(b)
-g.size() == (4, 1)
-result = g.size()
-result
+assert g.size() == (4, 1)
 
+b = SimpleGraph()
+l1 = Vertex('l1')
+l2 = Vertex('l2')
+l3 = Vertex('l3')
+l4 = Vertex('l4')
+r1 = Vertex('r1')
+r2 = Vertex('r2')
+r3 = Vertex('r3')
 
-""""""
+b.add_vertex(l1)
+b.add_vertex(l2)
+b.add_vertex(l3)
+b.add_vertex(l4)
+b.add_vertex(r1)
+b.add_vertex(r2)
+b.add_vertex(r3)
+b.add_edge(l1, r1)
+b.add_edge(l1, r2)
+b.add_edge(l1, r3)
+b.add_edge(l2, r1)
+b.add_edge(l2, r2)
+b.add_edge(l2, r3)
+b.add_edge(l3, r1)
+b.add_edge(l3, r2)
+b.add_edge(l3, r3)
+b.add_edge(l4, r1)
+b.add_edge(l4, r2)
+b.add_edge(l4, r3)
+
+assert b.is_bipartite()
+bl1 = Vertex('bl1')
+bl2 = Vertex('bl2')
+br1 = Vertex('br1')
+b.add_vertex(bl1)
+b.add_vertex(bl2)
+b.add_vertex(br1)
+b.add_edge(bl1, br1)
+b.add_edge(bl2, br1)
+assert b.is_bipartite()
+
+c1 = Vertex('c1')
+b.add_vertex(c1)
+b.add_edge(l1, c1)
+b.add_edge(r1, c1)
+assert not b.is_bipartite()
+
